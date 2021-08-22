1a1e4c3e0603f5151441e5f62877120e06c8b3bf
brocklarson <brock.larson0725@gmail.com>
2021-08-22 15:08:43 -0600

    Add logic for pressing button to play game

--- js/main.js
+++ js/main.js
@@ -1,68 +1,113 @@
 const buttons = document.querySelectorAll('button');
+const playerScoreText = document.getElementById('playerScore');
+const computerScoreText = document.getElementById('computerScore');
+const playerSelectionIcon = document.getElementById('playerSelectionIcon');
+const computerSelectionIcon = document.getElementById('computerSelectionIcon');
+const roundWinLoseText = document.getElementById('roundWinLoseText');
+const screenOverlay = document.getElementById('screenOverlay');
+const endGameContainer = document.getElementById('endGameContainer');
+const restartButton = document.getElementById('restartButton');
+const gameWinLoseText = document.getElementById('gameWinLoseText');
+const finalScoreText = document.getElementById('gameFinalScore');
+let playerScore = 0;
+let computerScore = 0;
+
 
 function computerPlay(){
     let computerSelection = Math.floor(Math.random()*3);
-    if(computerSelection === 0) return `rock`;
-    if(computerSelection === 1) return `paper`;
-    return `scissors`;
-}
-
-function playerPlay(buttonID){
-    if(buttonID === 'rockButton') return `rock`;
-    if(buttonID === `paperButton`) return `paper`;
-    return `scissors`;
-}
-
-function playRound(computerSelection, playerSelection){
-    if(computerSelection === `rock`){
-        if(playerSelection === `rock`){
-            return `Tie! You both played Rock. Go again`;
-        }else if(playerSelection === `paper`){
-            return `You Win! Paper beats Rock`;
-        }else{
-            return `You Lose! Rock beats Scissors`;
-        }
-    }else if(computerSelection === `paper`){
-        if(playerSelection === `rock`){
-            return `You Lose! Paper beats Rock`;
-        }else if(playerSelection === `paper`){
-            return `Tie! You both played Paper. Go again`;
-        }else{
-            return `You Win! Scissors beats Paper`;
-        }
+    if(computerSelection === 0){
+        computerSelectionIcon.src = 'icon/rock-no-bg.png';
+        return `rock`;
+    }else if(computerSelection === 1){
+        computerSelectionIcon.src = 'icon/paper-no-bg.png';
+        return `paper`;
     }else{
-        if(playerSelection === `rock`){
-            return `You Win! Rock beats Scissors`;
-        }else if(playerSelection === `paper`){
-            return `You Lose! Scissors beats Paper`;
-        }else{
-            return `Tie! You both played Scissors.  Go again`;
-        }
+        computerSelectionIcon.src = 'icon/scissors-no-bg.png';
+        return `scissors`;
     }
 }
 
-
-function game(buttonID){
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    const computerSelection = computerPlay();
-    const playerSelection = playerPlay(buttonID);
-    
-    const roundWinner = playRound(computerSelection, playerSelection);
-    console.log(`Round ${i+1}: ${roundWinner}`);
-    if(roundWinner.substring(4,7) === `Win`) playerScore += 1;
-    if(roundWinner.substring(4,8) === `Lose`) computerScore += 1;
-    if(roundWinner.substring(0,3) === `Tie`) i -= 1;
-    if(roundWinner.substring(0,3) !== `Tie`) console.log(`The current ` +
-            `score is: You - ${playerScore}, Computer - ${computerScore}`);
-    
+function playerPlay(buttonID){
+    if(buttonID === 'rockButton'){ 
+        playerSelectionIcon.src = 'icon/rock-no-bg.png';
+        return `rock`;
+    }else if(buttonID === `paperButton`){
+        playerSelectionIcon.src = 'icon/paper-no-bg.png';
+        return `paper`;
+    }else{
+        playerSelectionIcon.src = 'icon/scissors-no-bg.png';
+        return `scissors`;
+    }
 }
 
-//game();
+function playRound(computerSelection, playerSelection){
+    if((playerSelection === `rock` && computerSelection === `scissors`) ||
+        (playerSelection === `paper` && computerSelection === `rock`) ||
+        (playerSelection === `scissors` && computerSelection === `paper`)){
+            return `You Win!`;
+    }else if((playerSelection === `rock` && computerSelection === `paper`) ||
+        (playerSelection === `paper` && computerSelection === `scissors`) ||
+        (playerSelection === `scissors` && computerSelection === `rock`)){
+            return `You Lost`;
+    }else{
+        return `Tie`;
+    }
+}
 
+function updateScore(roundWinner){
+    roundWinLoseText.innerText = roundWinner;
+    if(roundWinner === `You Win!`) {
+        playerScore += 1;
+        playerScoreText.innerText = playerScore.toString();
+    }else if(roundWinner === `You Lost`) {
+        computerScore += 1;
+        computerScoreText.innerText = computerScore.toString();
+    }
+
+    if(playerScore >= 5 || computerScore >= 5){
+        endGame(playerScore, computerScore);
+    }
+}
+
+function endGame(playerScore, computerScore){
+    if(playerScore > computerScore){
+        gameWinLoseText.innerText = `You Win!`;
+    }else{
+        gameWinLoseText.innerText = `You Lose`;
+    }
+    finalScoreText.innerText = `${playerScore} - ${computerScore}`;
+    screenOverlay.classList.add('display');
+    endGameContainer.classList.add('display');
+}
+
+function game(buttonID){
+    playerSelectionIcon.src = '';
+    computerSelectionIcon.src = '';
+    const computerSelection = computerPlay();
+    const playerSelection = playerPlay(buttonID);
+    const roundWinner = playRound(computerSelection, playerSelection);
+    updateScore(roundWinner); 
+}
+
+function initializeGame(){
+    playerScore = 0;
+    computerScore = 0;
+
+    playerScoreText.innerText = playerScore.toString();
+    computerScoreText.innerText = computerScore.toString();
+    screenOverlay.classList.remove('display');
+    endGameContainer.classList.remove('display');
+    roundWinLoseText.innerText = '';
+    playerSelectionIcon.src = '';
+    computerSelectionIcon.src = '';
+}
+
+initializeGame();
+
+//Event Listeners
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         game(button.id)
     });
 });
+restartButton.addEventListener('click', initializeGame);
